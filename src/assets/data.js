97c3051bdebf68646bca0902376a922fef5694c1
Scholar <89717401+mjmartey19@users.noreply.github.com>
2023-11-17 15:27:24 +0000

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,28 @@
+export const user =
+  {
+    _id: "84df3c064h180b81adfe41d4b",
+      userName: "Daniel123",
+      firstName: "Daniel",
+      lastName: "Boadu",
+      email: "danielboadu594@gmail.com",
+      verified: false,
+  plan: "free",
+  createdAt: "2023-08-18T09:38:14.179Z",
+  updatedAt: "2023-08-21T06:46:18.258Z",
+  profileUrl: "https://res.cloudinary.com/djs3wu5bg/image/upload/v1683874470/cld-sample.jpg",
+}
+
+
+
+// {
+//   _id: "84df3c064h180b81adfe41d4b",
+//   firstName: "Daniel",
+//   lastName: "Boadu",
+//   email: "danielBoadu594@gmail.com",
+//   verified: false,
+//   createdAt: "2023-08-18T09:38:14.179Z",
+//   updatedAt: "2023-08-21T06:46:18.258Z",
+//   profileUrl:
+//     "https://res.cloudinary.com/djs3wu5bg/image/upload/v1683874454/samples/people/boy-snow-hoodie.jpg",
+//   token: "hZWFmZmU3NmMiguJpYXQiOjE2OTIwMzY5",
+// },
---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import NoProfile from "./userprofile.png";
+import kente1 from "./kent-1.jpg";
+import kente2 from "./kent-2.jpg";
+export { NoProfile, kente1, kente2 };
